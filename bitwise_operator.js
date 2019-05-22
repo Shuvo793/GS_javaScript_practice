374782ee22657efa46df48c04234ba13e33adf10
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-22 15:36:58 +0600

    bitwise operator

---
+++ bitwise_operator.js
@@ -0,0 +1,6 @@
+//bitwise operators:>>(shift toright),<<(shift to left),~(tilday),&(and),|(or),^(xor)
+
+var a= 10;//age binary formate ee nibe
+var b=30;//age binary formate ee nibe
+var c=a & b;// 2 ta binay formate moddhe logical operation haoar por (var c;) er moddhe decimal number assign hobe
+console.log(c);
