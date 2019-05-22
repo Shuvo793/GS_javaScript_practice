558276fe216acfdde2ffd4be39ec717cdfd9b016
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-22 15:04:24 +0600

    relational or comparission operatior

---
+++ Comparission_Relational_operator.js
@@ -0,0 +1,10 @@
+var a= 20;
+var b= '20';
+//var c= a>b;
+//var c= a<b
+//var c= a==b
+//var c= a!=b
+//var c= a==b output :true
+var c= a===b;
+console.log(c);
+ 
---
+++ arithmatic_operator.js
@@ -0,0 +1,19 @@
+/* var a=20,
+    b=30,
+    c=a+b;
+console.log(c); */
+
+/* var a=20,
+    b=30,
+    //c=a-b;
+    //c=a*b;
+    //c=a/b;
+    //c=a%b;
+console.log(c); */
+
+/* var a=20,
+    b=30;
+   // b++;
+   //b--;
+    
+console.log(b); */
