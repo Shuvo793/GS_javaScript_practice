80e42a6da9630b575f7e18cb690a0038ef56766e
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-22 14:45:44 +0600

    declaring variables

---
+++ declaring_variable.js
@@ -0,0 +1,20 @@
+/* var price;
+price=false;
+console.log(price); */
+
+
+/* var x,y,z;
+x=100;
+y=200;
+z=33;
+console.log(x); */
+
+// var x='shuvo',y='sathi';
+
+// console.log(x);
+
+
+var x='shuvo',
+    y='sathi',
+    z='frnd';
+console.log(z);
