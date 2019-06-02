83d02e40ce1bcac0261efc7cf0a639c4272dadfc
meSuvo <cse.engr.shuvo@gmail.com>
2019-06-02 10:22:02 +0600

    argument_object concept clear

---
+++ argument_object.js
@@ -0,0 +1,25 @@
+function mySelf(name1,name2){
+    //way 1
+   /* console.log(name1);
+   console.log(name2); */
+   
+   //way 2
+  /*  console.log(arguments[0]);
+   console.log(arguments[1]); */
+
+
+
+    //way 1
+   //console.log(arguments.length)
+
+   //way 2
+    /* var l = arguments.length;
+    console.log(l); */
+
+    //function details and output document.write() kore browser ee dakhte hobe
+    /* console.log(arguments.callee); */
+    
+
+
+}
+mySelf("hellow",'world');
