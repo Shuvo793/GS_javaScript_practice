8ba4c883d41da99a15eecc204bbabd4c8f4e2177
meSuvo <cse.engr.shuvo@gmail.com>
2019-06-04 01:11:51 +0600

    variable block scope concept

---
+++ variable_block_scope_feature.js
@@ -0,0 +1,15 @@
+if(true){
+    var i = 10;
+    console.log(i);
+}
+console.log(i);
+//javaScript ee block statment variable asign korle ta baire theke access kora jae jeta onno kono programming language ee access kora jae na.....er kisu shubidha ar osubidha oo ase...
+
+
+//block scope er jonno (Let) keyword ta use korte hoe
+//ex.
+if(true){
+    let j = 20;
+    console.log(j); //output : 20
+}
+console.log(j); //output : error-> j is undefined
