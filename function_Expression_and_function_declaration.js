11c26a0d222238c7660ebcbc83ecf3ecbaebe303
meSuvo <cse.engr.shuvo@gmail.com>
2019-06-04 01:40:08 +0600

    function expression and function declaration

---
+++ function_Expression_and_function_declaration.js
@@ -0,0 +1,25 @@
+
+
+/* function declaration ee amara jekono jaegate function call korte parbo kintu (function expression) ee shudhu matro function er niche call korte hobe karon function expretion ee variable hoisting kaj kore na */ 
+
+//function declaration
+name();
+function name(){
+    console.log('shuvo');
+}
+name();
+
+
+//function expression
+
+/* syntx: var myName = function name(){
+    //body
+}
+myName(); */
+
+//ex:
+
+var myName=function name(){
+    console.log("shuvo");
+}
+myName();//output:shuvo
