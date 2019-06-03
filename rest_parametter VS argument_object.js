236293779efac78a71db5fcabd8b1dbf27f32649
meSuvo <cse.engr.shuvo@gmail.com>
2019-06-04 00:48:22 +0600

    rest_parametter vs argument_object concept

---
+++ rest_parametter VS argument_object.js
@@ -0,0 +1,23 @@
+//rest_parametter
+function rest(a,...args){
+    console.log(a)
+    console.log(args);
+}
+rest(10,20,30,40);
+/* output : a = 10
+        0=20
+        1=30
+        2=40 */
+
+//arguments_object
+function arguments(a){
+    console.log(a)
+    console.log(arguments);
+}
+arguments(10,20,30,40);
+/* output : a = 10
+        0=10
+        1=20
+        2=30
+        3=40 */
+            
